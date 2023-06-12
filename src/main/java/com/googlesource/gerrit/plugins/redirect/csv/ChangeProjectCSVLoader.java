8999866b85b04b2be9995a58ae75fb8c693aeaa2
ALVARO VILAPLANA GARCIA <alvaro.vilaplana@gmail.com>
2023-06-12 15:27:45 +0000

    Redirects of /<change-num> to /<project>/<change-num>
    Change-Id: I5145fbc5286d2e9d2782ddfc949cc786c1172b67

---
+++ src/main/java/com/googlesource/gerrit/plugins/redirect/csv/ChangeProjectCSVLoader.java
@@ -0,0 +1,33 @@
+package com.googlesource.gerrit.plugins.redirect.csv;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChangeProjectCSVLoader {
+  public static Map<Integer, String> loadCSV(String filePath) {
+    Map<Integer, String> dataMap = new HashMap<>();
+
+    try (InputStream csvInputStream =
+            ChangeProjectCSVLoader.class.getClassLoader().getResourceAsStream(filePath);
+        BufferedReader br = new BufferedReader(new InputStreamReader(csvInputStream))) {
+      String line;
+      boolean isFirstLine = true;
+      while ((line = br.readLine()) != null) {
+        if (isFirstLine) {
+          isFirstLine = false;
+          continue; // Skip the header line
+        }
+
+        String[] columns = line.split(",");
+        int changeNumber = Integer.parseInt(columns[0]);
+        String projectName = columns[1];
+        dataMap.put(changeNumber, projectName);
+      }
+    } catch (Exception e) {
+      throw new CSVProcessingException(String.format("Error processing CSV file: %s", filePath), e);
+    }
+
+    return dataMap;
+  }
+}
